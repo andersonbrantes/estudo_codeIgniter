dd324dafee0f0d92b62ba7dbe24525c7f0cb9e36
AndersonBrantes <andersonbrantes@gmail.com>
2015-04-19 21:08:11 -0300

    adicionando database ao autoload / e habilitando o profiling para testar

--- application/controllers/produtos.php
+++ application/controllers/produtos.php
@@ -4,16 +4,15 @@ class Produtos extends CI_Controller{
 
 	public function index() {
 
-		$this->load->database();
+		$this->output->enable_profiler(TRUE);
+
 		$this->load->model("produtos_model");
 
 		$produtos = $this->produtos_model->buscaTodos();
 
 		$dados = array("produtos" => $produtos);
 
-		$this->load->helper("url");
-		$this->load->helper("currency");
-		$this->load->helper("form");
+		$this->load->helper(array("url", "currency", "form") );
 
 		$this->load->view("produtos/index.php", $dados);
 		
