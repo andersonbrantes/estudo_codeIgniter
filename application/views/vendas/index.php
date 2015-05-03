bfdfc2583ea9e93521e8a030907b6b2d4efb8740
AndersonBrantes <andersonbrantes@gmail.com>
2015-05-02 22:12:20 -0300

    criando listagem dos produtos vendidos de um usuário

---
+++ application/views/vendas/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="<?= base_url("css/bootstrap.css"); ?>">
+	<title>Document</title>
+</head>
+<body>
+	
+	<div class="container">
+
+			<h1>Produtos</h1>
+			<table class="table">
+				<?php foreach($produtosVendidos as $produto) : ?>
+					<tr>
+						<td><?=$produto["nome"]?></td>
+						<td><?=$produto["data_da_entrega"]?></td>
+					</tr>
+				<?php endforeach ?>
+			</table>
+	</div>
+</body>
+</html>
