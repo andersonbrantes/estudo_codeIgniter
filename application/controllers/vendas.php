b49ec50ccab9339ca1614d74e927e90608e196b3
AndersonBrantes <andersonbrantes@gmail.com>
2015-05-04 23:27:19 -0300

    adicionando enviar email ao realizar compra de um produto

--- application/controllers/vendas.php
+++ application/controllers/vendas.php
@@ -6,13 +6,40 @@ class Vendas extends CI_Controller {
 
 		$usuario = autoriza();
 
-		$this->load->model("vendas_model");
+		$this->load->model(array("vendas_model", "produtos_model", "usuarios_model"));
 		$vendas = array(
 			"produto_id" => $this->input->post("produto_id"),
 			"comprador_id" => $usuario["id"],
 			"data_da_entrega" => dataPtBrParaMysql($this->input->post("data_de_entrega"))
 		);
 		$this->vendas_model->salva($vendas);
+
+		$this->load->library("email");
+		$config["protocol"] = "smtp";
+		$config["smtp_host"] = "ssl://smtp.gmail.com";
+		$config["smtp_user"] = "andersonbrantes@gmail.com";
+		$config["smtp_pass"] = "ccvfvfdc43=43";
+		$config["smtp_port"] = "465";
+		$config["charset"] = "utf-8";
+		$config["mailtype"] = "html";
+		$config["newline"] = "\r\n";
+		$this->email->initialize($config);
+
+
+		$produtos = $this->produtos_model->busca($venda["produto_id"]);
+		$vendedor = $this->produtos_model->busca($produto["usuario_id"]);
+
+		$dados = array("produto" => $produto);
+		$conteudo = $this->load->view("vendas/email.php", $dados, TRUE);
+
+		$this->email->from("andersonbrantes@gmail", "Mercado");
+		$this->email->to(array($vendedor['email']));
+		$this->email->subject("Seu produto {$produto['nome']} foi vendido!");
+		$this->email->message($conteudo);
+		$this->email->send();
+
+
+
 		$this->session->set_flashdata("success", "Pedido de compra efetuado com sucesso!");
 		redirect("/");
 	}
--- application/models/usuarios_model.php
+++ application/models/usuarios_model.php
@@ -14,4 +14,10 @@ class Usuarios_model extends CI_Model {
 		$usuario = $this->db->get("usuarios")->row_array();
 		return $usuario;
 	}
+
+	public function busca($id) {
+
+		$this->db->where("id", $id);
+		return $this->db->get("usuarios")->row_array();
+	}
 }
